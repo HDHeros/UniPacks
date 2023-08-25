1125504b3a1307496a423c68f068d06137d753b3
HDHero <leodenx@gmail.com>
2023-08-25 10:31:41 +0200

    fix: IStateSwitcher interface

--- Packages/com.hdhero.fsm/Runtime/IStateSwitcher.cs
+++ Packages/com.hdhero.fsm/Runtime/IStateSwitcher.cs
@@ -1,7 +1,9 @@
+using System;
+
 namespace HDH.Fsm
 {
     public interface IStateSwitcher
     {
-        public void SwitchState<TNewState>() where TNewState : SwitchableState;
+        public void SwitchState<TNewState>(Action callback = null) where TNewState : SwitchableState;
     }
 }
