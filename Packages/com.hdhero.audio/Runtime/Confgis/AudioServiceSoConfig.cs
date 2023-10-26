22bc090af608d7d9fe824ccca64ebbe40a21b75c
HDHero <leodenx@gmail.com>
2023-10-26 13:13:45 +0200

    feat: AudioServiceSoConfig.cs

---
+++ Packages/com.hdhero.audio/Runtime/Confgis/AudioServiceSoConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace HDH.Audio.Confgis
+{
+    [CreateAssetMenu(menuName = "HDH/Audio/AudioServiceConfig")]
+    public class AudioServiceSoConfig : ScriptableObject
+    {
+        public AudioServiceConfig Config;
+    }
+}
