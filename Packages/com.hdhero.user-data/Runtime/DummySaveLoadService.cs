5bc455c71c7f59f4aa82f18db70d0332596c762d
HDHero <leodenx@gmail.com>
2024-01-15 14:29:50 +0100

    feat: dummy save load service

---
+++ Packages/com.hdhero.user-data/Runtime/DummySaveLoadService.cs
@@ -0,0 +1,13 @@
+﻿namespace HDH.UserData
+{
+    public class DummySaveLoadService : ISaveLoadService
+    {
+        public void Save<T>(T model) where T : DataModel, new()
+        {
+
+        }
+
+        public T Load<T>() where T : DataModel, new() => 
+            new T();
+    }
+}
