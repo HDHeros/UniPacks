168cf77f8347c2a7cab0c246fb3e78549e3a3b2f
HDHero <leodenx@gmail.com>
2024-03-13 09:39:50 +0100

    feat: SwitchState by type for Fsm StateSwitcher

--- Packages/com.hdhero.fsm/Runtime/IStateSwitcher.cs
+++ Packages/com.hdhero.fsm/Runtime/IStateSwitcher.cs
@@ -5,5 +5,6 @@ namespace HDH.Fsm
     public interface IStateSwitcher
     {
         public void SwitchState<TNewState>(Action callback = null) where TNewState : SwitchableState;
+        public void SwitchState(Type stateType, Action callback = null);
     }
 }
