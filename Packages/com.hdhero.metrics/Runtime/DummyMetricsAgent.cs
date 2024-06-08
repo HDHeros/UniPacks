84e4781fe66cbd59f8f511991c7b877b94a71f42
HDHero <leodenx@gmail.com>
2024-06-08 09:51:26 +0200

    [metrics] feat: dummy metrics agent

---
+++ Packages/com.hdhero.metrics/Runtime/DummyMetricsAgent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace HDH.Metrics
+{
+    public class DummyMetricsAgent : IMetricsAgent
+    {
+        public void TrackEvent(string eventName)
+        {
+        }
+
+        public void TrackEvent(string eventName, Dictionary<string, object> @params)
+        {
+        }
+
+        public void TrackEventOnce(string eventName)
+        {
+        }
+
+        public void TrackEventOnce(string eventName, Dictionary<string, object> @params)
+        {
+        }
+    }
+}
